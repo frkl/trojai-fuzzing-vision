7cec4182eca6bda46d6b693e49c62b0db2791c37
frkl <linxiao.frank@gmail.com>
2023-05-07 13:57:16 -0700

    Add annotations to perm_inv.py

--- util/perm_inv.py
+++ util/perm_inv.py
@@ -7,12 +7,11 @@ import itertools
 import string
 import numpy
 
-
+#See Line 442 for usage
 
 def powerset(n):
     s = list(range(n))
     return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
-
 
 def calculate_affix_combos(N,K):
     if K==1:
@@ -73,23 +72,6 @@ def breakable_n(comp):
     
     return False
 
-def count(x):
-    c={}
-    for v in x:
-        if not v in c:  
-            c[v]=0
-        
-        c[v]=c[v]+1
-    
-    c2={}
-    for v in c:
-        if not c[v] in c2:
-            c2[c[v]]=[v]
-        else:
-            c2[c[v]].append(v)
-    
-    return c2;
-
 def remap(affix):
     #list all alternative numbering schemes
     v=set(list(affix))
@@ -120,6 +102,104 @@ def normalize_n(comp):
     m=tuple(zip(*m))
     return m
 
+#Computes all possible permutation invariant terms for 2-dim inputs at order-n
+#Terms are represented in a prefix-suffix format
+#e.g. an order-5 term ((0,0,0,1,1),(0,0,1,0,1)) stands for sum_abcd Xac Xac Xad Xbc Xbd
+def generate_comps(n=5):
+    #List all possible prefixes and suffixes
+    prefix=calculate_affix_combos(n,n)
+    suffix=calculate_affix_combos(n,n)
+    
+    #Enumerate all prefix-suffix combinations
+    comps=list(itertools.product(prefix,suffix))
+    #print(len(comps))
+    
+    #Normalize their combination to a "smallest" normal form.
+    #What happened in normalize() was I enumerated all permutations, did a "sort" and take the minimum
+    comps_n=list(set([normalize(p,s) for p,s in comps]))
+    
+    #Filter combinations to keep only the ones that cannot be broken into lower order terms
+    comps_=[(p,s) for p,s in comps_n if not breakable(p,s)]
+    comps_=list(set(comps_))
+    return comps_
+
+#Computes all possible permutation invariant terms for k-dim inputs at order-n
+#Terms are represented in a prefix-suffix format
+#e.g. a 3-dim order-4 term ((0,0,1,1),(0,1,0,1),(0,1,1,0)) stands for sum_abcdef Xace Xadf Xbcf Xbde
+#Same algorithm as above
+def generate_comps_n(n=5,k=3):
+    comps=[calculate_affix_combos(n,n) for i in range(k)]
+    comps=list(itertools.product(*comps))
+    comps_n=[];
+    for i,comp in enumerate(comps):
+        if i%1000==0:
+            print('Normalizing comps %d/%d'%(i,len(comps)),end='\r')
+        
+        comps_n.append(normalize_n(comp))
+    
+    print('');
+    comps_n=list(set(comps_n))
+    print('%d remains after normalization'%(len(comps_n)))
+    comps_=[comp for comp in comps_n if not breakable_n(comp)]
+    comps_=list(set(comps_))
+    print('%d final'%(len(comps_)))
+    return comps_
+
+
+
+'''
+
+comps=[];
+for i in range(1,6):
+    print(i)
+    comps+=generate_comps(i)
+
+net=perm_inv2(comps)
+net(torch.Tensor(3,15,10))
+
+comps_paired=[]
+for prefix,suffix in comps:
+    comps_paired+=validate_comps_paired(prefix,suffix,2)
+
+net=perm_inv2_paired_suffix(comps_paired)
+net((torch.Tensor(3,15,10),torch.Tensor(3,8,10)))
+
+
+
+
+paths=[einopt(*comp) for comp in comps]
+[p for p in paths if p[1]>1e9]
+
+
+print(len(comps_))
+
+path=einopt(*comps_[0])
+tmp=net(path[-1])
+x=torch.ones(1,100,120)
+y=tmp(x,x,x,x,x)
+
+[x for x in path[1].split('\n')[-1].split(' ') if not x=='']
+
+'''
+
+
+
+def count(x):
+    c={}
+    for v in x:
+        if not v in c:  
+            c[v]=0
+        
+        c[v]=c[v]+1
+    
+    c2={}
+    for v in c:
+        if not c[v] in c2:
+            c2[c[v]]=[v]
+        else:
+            c2[c[v]].append(v)
+    
+    return c2;
 
 def eliminate_singles(affix):
     c=count(affix)
@@ -136,7 +216,14 @@ def eliminate_singles(affix):
         return affix
 
 
-
+# Given a (prefix,suffix), generate the einsum string needed to calculate the feature
+# E.g. prefix (0,0,0,1,2), suffix (0,0,1,0,1), we should have einsum string ZaA,ZaA,ZaB,ZbA,ZcB->Z. Here Z is a batch dimension.
+#     Because our einsum optimization package seems to ignore the fact that here "c" should be processed first
+#     I also added optimizations that removes those literals that only appeared once
+#     einstr0 would be the original ZaA,ZaA,ZaB,ZbA,ZcB->Z
+#     einstr is the optimized  ZaA,ZaA,ZaB,ZbA,ZB->Z
+#     We need to know that we need to preprocess the fifth input using ZcB->ZB, so prepro is a list containing those preprocessings
+#     Our einsum optimization package wants the dimensionality of each input. So we need to tell it that ZaA would be X, but ZB would be X.sum(dim=0). We added an arr_type flag indicating what type of matrix, full, missing prefix, missing suffix or missing both, that each input is.
 def einstr(prefix_,suffix_):
     prefix_dict=string.ascii_lowercase
     suffix_dict=string.ascii_uppercase
@@ -225,9 +312,6 @@ def einstr_n(comp):
     return einstr,arr_type,prepro
 
 
-#einstr_n(((0, 0, 1, 1, 2), (0, 1, 0, 1, 0), (0, 1, 0, 1, 1)))
-
-
 
 def parse_line(line):
     return [x for x in line.split('\n')[-1].split(' ') if not x=='']
@@ -242,6 +326,16 @@ def parse_einpath(path):
 
 
 
+# Call numpy einsum_path to optimize einsum computation
+# If you send in ZaA,ZbB,ZaB,ZbA->Z naively, pytorch will always process the terms in-order.
+#      Like ZaA ZbB will give us a huge intermediate ZabAB
+# We should instead do
+#      ZaA ZaB -> ZAB
+#      ZAB ZbB -> ZbA
+#      ZbA ZbA -> Z
+#  numpy.einsum_path does this kind of optimization
+#  We parse the "paths" that it generates, basically the smaller einsum operations
+#  And executes those operations in the "einnet" class
 def einopt(prefix,suffix,w=1000,h=1000):
     n=len(prefix)
     s0,s,t,prepro=einstr(prefix,suffix)
@@ -315,11 +409,10 @@ def ein_slice(s,*ops,sz=1000,one=False):
     
     return slices
 
-
 #out=ein_slice('zij,zij->z',torch.Tensor(1,5000,3000),torch.Tensor(1,5000,3000),sz=1000)
 
 
-
+#Executes a series of einsum operations coming out of optimization
 class einnet(nn.Module):
     def __init__(self,cmd):
         super().__init__()
@@ -346,6 +439,33 @@ class einnet(nn.Module):
         
         return y
 
+'''
+Finally, our pytorch network that extracts features for 2D permutation invariance
+Takes comps, which is a list of (prefix,suffix) representation of the features
+w and h are *expected* size of the input x: torch.Tensor(batch_size,h,w). This is only for compute and memory optimization purposes. Actual input can be of different sizes
+This class will generate optimized einsum paths for each feature, and run them.
+
+Usage example:
+
+import torch
+import util.perm_inv as inv
+#Generate 2-dim invariant features up to order-5 
+comps=[];
+for i in range(1,6):
+    print(i)
+    comps+=inv.generate_comps(i)
+
+print(len(comps)) # 67 features from order-1 to order-5
+
+net=inv.perm_inv2(comps)
+
+#Now network output will be invariant to permutations to dimensions in x  (mostly, but small errors in floating point)
+x=torch.rand(3,15,10).double()
+y0=net(x)
+y1=net(x[:,torch.randperm(15),:][:,:,torch.randperm(10)])
+print((y0-y1).abs().max()) # tensor(1.6653e-16, dtype=torch.float64)
+
+'''
 class perm_inv2(nn.Module):
     def __init__(self,comps,w=1000,h=1000):
         super().__init__()
@@ -390,6 +510,8 @@ class perm_inv2(nn.Module):
             if sz is None:
                 h=net(*ops)
             else:
+                #Some einsum operations take little compute but will run out of GRAM 
+                #Solution is to slice the input and compute einsum on multiple slices separately
                 ops_slices=ein_slice(self.einstrs[i],*ops,sz=sz)
                 h=[net(*ops,sz=sz,device=device) for ops in ops_slices]
                 
@@ -490,74 +612,4 @@ def validate_comps_paired(prefix,suffix,k=2):
     return valid_cfgs
 
 
-    
-
-def generate_comps(n=5):
-    prefix=calculate_affix_combos(n,n)
-    suffix=calculate_affix_combos(n,n)
-    
-    comps=list(itertools.product(prefix,suffix))
-    #print(len(comps))
-    
-    comps_n=list(set([normalize(p,s) for p,s in comps]))
-    comps_=[(p,s) for p,s in comps_n if not breakable(p,s)]
-    
-    
-    comps_=list(set(comps_))
-    return comps_
-
-def generate_comps_n(n=5,k=3):
-    comps=[calculate_affix_combos(n,n) for i in range(k)]
-    comps=list(itertools.product(*comps))
-    comps_n=[];
-    for i,comp in enumerate(comps):
-        if i%1000==0:
-            print('Normalizing comps %d/%d'%(i,len(comps)),end='\r')
-        
-        comps_n.append(normalize_n(comp))
-    
-    print('');
-    comps_n=list(set(comps_n))
-    print('%d remains after normalization'%(len(comps_n)))
-    comps_=[comp for comp in comps_n if not breakable_n(comp)]
-    comps_=list(set(comps_))
-    print('%d final'%(len(comps_)))
-    return comps_
-
-
-
-'''
-
-comps=[];
-for i in range(1,6):
-    print(i)
-    comps+=generate_comps(i)
-
-net=perm_inv2(comps)
-net(torch.Tensor(3,15,10))
-
-comps_paired=[]
-for prefix,suffix in comps:
-    comps_paired+=validate_comps_paired(prefix,suffix,2)
-
-net=perm_inv2_paired_suffix(comps_paired)
-net((torch.Tensor(3,15,10),torch.Tensor(3,8,10)))
-
-
-
-
-paths=[einopt(*comp) for comp in comps]
-[p for p in paths if p[1]>1e9]
-
-
-print(len(comps_))
-
-path=einopt(*comps_[0])
-tmp=net(path[-1])
-x=torch.ones(1,100,120)
-y=tmp(x,x,x,x,x)
-
-[x for x in path[1].split('\n')[-1].split(' ') if not x=='']
-
-'''
 
